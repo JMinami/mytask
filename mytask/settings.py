5cfd6fbbf1b8927717858d31e0cf057dc4851e10
JMinami <mj14rugby@yahoo.co.jp>
2019-11-02 16:25:08 +0900

    test

--- mytask/settings.py
+++ mytask/settings.py
@@ -133,10 +133,11 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-if not DEBUG:
-    STATIC_URL = '/static/'
+
+if DEBUG:
+    STATIC_URL = '/staticfiles/'
 else:
-    STATIC_URL = os.path.join(BASE_DIR, '/staticfiles/')
+    STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'mng/static'),
